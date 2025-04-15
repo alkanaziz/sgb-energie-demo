d10e73cd8f31abe47e91072937452fafced16699
Alkan Aziz <alkanaziz@gmail.com>
2025-04-15 08:21:20 +0200

    Fix Sidebar background color to match design specifications

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,7 +5,7 @@ import { TfiAnnouncement, TfiEmail } from "react-icons/tfi";
 const Sidebar = () => {
   return (
     <nav className="fixed bottom-0 z-30 h-[10vh] w-full md:right-0 md:h-screen md:w-0">
-      <div className="right-0 flex h-full items-center justify-between bg-green-700 px-15 text-white md:fixed md:top-1/2 md:h-48 md:w-[56px] md:-translate-y-1/2 md:transform md:flex-col md:gap-8 md:bg-transparent md:px-3">
+      <div className="right-0 flex h-full items-center justify-between bg-[#94C11F] px-15 text-white md:fixed md:top-1/2 md:h-48 md:w-[56px] md:-translate-y-1/2 md:transform md:flex-col md:gap-8 md:bg-transparent md:px-3">
         <div className="flex h-[56px] w-[56px] items-center justify-center md:relative md:mb-8 md:w-full">
           <Link
             href="/news"
